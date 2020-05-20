7378cfebe9aeaa025ce04dd641d1caebb0498243
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-20 14:37:59 +0200

    Add common types

---
+++ src/srv/InstantJob/SharedKernel/Types/BaseDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SharedKernel.Types
+{
+    public abstract class BaseDomainEvent
+    {
+        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/srv/InstantJob/SharedKernel/Types/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SharedKernel.Types
+{
+    public abstract class BaseEntity<TId>
+    {
+        public virtual TId Id { get; set; }
+
+        public virtual List<BaseDomainEvent> Events { get; set; } = new List<BaseDomainEvent>();
+    }
+}
