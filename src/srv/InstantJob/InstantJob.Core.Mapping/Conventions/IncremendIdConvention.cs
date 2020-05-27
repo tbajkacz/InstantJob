005192426a9903fe3f4bc4228d252303396f0d47
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-27 13:43:59 +0200

    Add IncrementIdConvention

---
+++ src/srv/InstantJob/InstantJob.Core.Mapping/Conventions/IncremendIdConvention.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using System;
+using System.Linq;
+
+namespace InstantJob.Core.NHibernate.Conventions
+{
+    public class IncrementIdConvention : IIdConvention
+    {
+        public void Apply(IIdentityInstance instance)
+        {
+            if (!instance.Type.Name.Contains("string", StringComparison.InvariantCultureIgnoreCase))
+            {
+                instance.GeneratedBy.Increment();
+            }
+        }
+    }
+}
