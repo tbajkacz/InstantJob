5b0c8a8e3e1489f34ef40028158c3bca2f4431de
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-27 13:44:16 +0200

    Add InstantJobTableNameConvention

---
+++ src/srv/InstantJob/InstantJob.Core.Mapping/Conventions/InstantJobTableNameConventions.cs
@@ -0,0 +1,13 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+
+namespace InstantJob.Core.NHibernate.Conventions
+{
+    public class InstantJobTableNameConventions : IClassConvention
+    {
+        public void Apply(IClassInstance instance)
+        {
+            instance.Table("IJ_" + instance.TableName);
+        }
+    }
+}
