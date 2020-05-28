50973b1b57f90a1db49561d3b0eb9dd546fed992
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:51:17 +0200

    Add data dependency injection

---
+++ src/srv/InstantJob/InstantJob.Core.Mapping/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using InstantJob.Core.NHibernate.Conventions;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate.Tool.hbm2ddl;
+using System.Data;
+
+namespace InstantJob.Core.NHibernate
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString)
+        {
+            var fact = Fluently.Configure()
+                .Database(SQLiteConfiguration.Standard.UsingFile(connectionString)
+                                                      .IsolationLevel(IsolationLevel.ReadCommitted))
+                .Mappings(x => x.FluentMappings.AddFromAssembly(typeof(DependencyInjection).Assembly)
+                                .Conventions.Add<IncrementIdConvention>()
+                                .Conventions.Add<InstantJobTableNameConvention>())
+                .ExposeConfiguration(x =>
+                    new SchemaUpdate(x).Execute(false, true))
+                .BuildSessionFactory();
+            return services.AddScoped(p => fact.OpenSession());
+        }
+    }
+}
