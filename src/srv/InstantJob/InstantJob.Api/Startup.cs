b91cd8acf51b4a873eb4dd44b1aeb44332b16209
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:52:39 +0200

    Register necessary app services

--- src/srv/InstantJob/InstantJob.Api/Startup.cs
+++ src/srv/InstantJob/InstantJob.Api/Startup.cs
@@ -1,6 +1,10 @@
+using InstantJob.Api.Extensions;
+using InstantJob.Core.NHibernate;
+using InstantJob.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,8 +12,22 @@ namespace InstantJob.Api
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            this.configuration = configuration;
+            this.env = env;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPersistence(configuration.GetConnectionString("Database"));
+            services.AddInfrastructure();
+            services.AddCookieAuthentication(env.IsDevelopment() ? CookieSecurePolicy.None : CookieSecurePolicy.Always);
+            services.AddAuthorizationWithPolicies();
+
             services.AddControllers();
         }
 
@@ -22,12 +40,12 @@ namespace InstantJob.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
