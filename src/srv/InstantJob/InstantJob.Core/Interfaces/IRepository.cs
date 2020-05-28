05b64503e8dced9c660235f2d4c020b9726703c4
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:48:47 +0200

    Add repository base types

---
+++ src/srv/InstantJob/InstantJob.Core/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using SharedKernel.Types;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InstantJob.Core.Interfaces
+{
+    public interface IRepository<TEntity, TId> where TEntity : BaseEntity<TId>
+    {
+        IEnumerable<TEntity> Get();
+
+        TEntity GetById(TId id);
+
+        TEntity GetByIdOrDefault(TId id);
+
+        Task<TEntity> GetByIdAsync(TId id);
+
+        Task<TEntity> GetByIdOrDefaultAsync(TId id);
+
+        Task AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TId id);
+    }
+}
