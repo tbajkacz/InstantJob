1c6bdf8d6bc2d2aef1817f8636326f9fcded8c3e
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:51:23 +0200

    Add infra dependency injection

---
+++ src/srv/InstantJob/InstantJob.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using InstantJob.Core.Interfaces;
+using InstantJob.Infrastructure.Data;
+using InstantJob.Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InstantJob.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+            => services.AddScoped<IUnitOfWork, NHibernateUnitOfWork>()
+                       .AddScoped<IUserRepository, NHibernateUserRepository>()
+                       .AddSingleton<IHashService, HashService>();
+    }
+}
