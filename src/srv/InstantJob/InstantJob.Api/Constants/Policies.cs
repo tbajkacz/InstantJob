c533da3a0e10cb4f64b4cde7b566da0134fbc10e
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:51:35 +0200

    Add policies

---
+++ src/srv/InstantJob/InstantJob.Api/Constants/Policies.cs
@@ -0,0 +1,9 @@
+﻿namespace InstantJob.Api.Constants
+{
+    public class Policies
+    {
+        public const string Administrator = "Admin";
+        public const string Mandatee = "Mandatee";
+        public const string Mandator = "Mandator";
+    }
+}
