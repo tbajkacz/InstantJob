a92b83c37a8156178a0083a56162f7794f1012ba
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:49:43 +0200

    Add missing exception

---
+++ src/srv/InstantJob/InstantJob.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InstantJob.Core.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(Type entityType, object id)
+            : base($"Entity of type {entityType.FullName} with id {id} was not found")
+        {
+        }
+    }
+}
