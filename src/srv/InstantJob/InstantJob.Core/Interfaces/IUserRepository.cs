ef6b50610e43b581f810802f61bb3e46102f901a
Tomasz Bajkacz <menidas98@gmail.com>
2020-05-28 23:51:05 +0200

    Add user repository types

---
+++ src/srv/InstantJob/InstantJob.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using InstantJob.Core.Dtos;
+using InstantJob.Core.Entities.UserAggregate;
+using System.Threading.Tasks;
+
+namespace InstantJob.Core.Interfaces
+{
+    public interface IUserRepository : IRepository<User, int>
+    {
+        Task<User> ValidateCredentialsAsync(UserAuthParams param);
+
+        Task UpdatePasswordAsync(UserUpdatePasswordParams param);
+    }
+}
---
+++ src/srv/InstantJob/InstantJob.Infrastructure/Data/NHibernateUserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using InstantJob.Core.Dtos;
+using InstantJob.Core.Entities.UserAggregate;
+using InstantJob.Core.Interfaces;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace InstantJob.Infrastructure.Data
+{
+    public class NHibernateUserRepository : NHibernateRepositoryBase<User, int>, IUserRepository
+    {
+        private readonly IHashService hashService;
+
+        public NHibernateUserRepository(ISession session, IHashService hashService)
+            : base(session)
+        {
+            this.hashService = hashService;
+        }
+
+        public async Task UpdatePasswordAsync(UserUpdatePasswordParams param)
+        {
+            var user = await GetByIdAsync(param.Id);
+            user.PasswordHash = hashService.Hash(param.Password);
+            await UpdateAsync(user);
+        }
+
+        public async Task<User> ValidateCredentialsAsync(UserAuthParams param)
+        {
+            var user = await session.Query<User>()
+                .SingleOrDefaultAsync(u => u.Email == param.Email);
+
+            return user == null ? null : hashService.CompareHashes(param.Password, user.PasswordHash) ? user : null;
+        }
+    }
+}
