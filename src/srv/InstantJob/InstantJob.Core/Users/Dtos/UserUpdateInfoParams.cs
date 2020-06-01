2baa471b64121bb78143cc768315cf695b816be1
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-01 21:33:27 +0200

    Add missing dto

---
+++ src/srv/InstantJob/InstantJob.Core/Users/Dtos/UserUpdateInfoParams.cs
@@ -0,0 +1,13 @@
+﻿namespace InstantJob.Core.Users.Dtos
+{
+    public class UserUpdateInfoParams
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Surname { get; set; }
+
+        public virtual int? Age { get; set; }
+
+        public virtual string Picture { get; set; }
+    }
+}
