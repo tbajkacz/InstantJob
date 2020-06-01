1817738663cbd0629876e7db5aa5609cf50aa5db
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-01 17:53:18 +0200

    Update infra dependency injection

--- src/srv/InstantJob/InstantJob.Infrastructure/DependencyInjection.cs
+++ src/srv/InstantJob/InstantJob.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,20 @@
-﻿using InstantJob.Core.Interfaces;
+﻿using InstantJob.Core.Common.Interfaces;
 using InstantJob.Infrastructure.Data;
+using InstantJob.Infrastructure.Identity;
 using InstantJob.Infrastructure.Security;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InstantJob.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
             => services.AddScoped<IUnitOfWork, NHibernateUnitOfWork>()
                        .AddScoped<IUserRepository, NHibernateUserRepository>()
-                       .AddSingleton<IHashService, HashService>();
+                       .AddSingleton<IHashService, HashService>()
+                       .Configure<HashOptions>(configuration.GetSection("Hash"))
+                       .AddScoped<IUserManager, UserManager>()
+                       .AddScoped<IDataSeeder, DefaultDataSeeder>();
     }
 }
