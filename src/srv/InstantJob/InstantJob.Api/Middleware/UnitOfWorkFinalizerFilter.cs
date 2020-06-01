c3a83ab976f6c7ae80a916d8694e23aa38e03c0c
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-01 19:37:27 +0200

    Add filter to finalize unit of work

---
+++ src/srv/InstantJob/InstantJob.Api/Middleware/UnitOfWorkFinalizerFilter.cs
@@ -0,0 +1,46 @@
+﻿using InstantJob.Core.Common.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace InstantJob.Api.Middleware
+{
+    public class UnitOfWorkFinalizerFilter : IAsyncActionFilter
+    {
+        private readonly IUnitOfWork uow;
+
+        public UnitOfWorkFinalizerFilter(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!uow.Active)
+            {
+                throw new Exception("Unit of work connection is already closed");
+            }
+
+            var executedContext = await next.Invoke();
+
+            if (executedContext.Exception == null)
+            {
+                await uow.CommitAsync();
+            }
+            else
+            {
+                await uow.RollbackAsync();
+            }
+        }
+    }
+
+    public static class IMvcBuilderExtensions
+    {
+        public static IMvcBuilder AddUnitOfWorkFinalizerFilter(this IMvcBuilder builder)
+        {
+            return builder.AddMvcOptions(o => o.Filters.Add<UnitOfWorkFinalizerFilter>());
+        }
+    }
+}
