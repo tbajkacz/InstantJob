38a357942053af9a6380236e6d35d7f032b0f008
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-01 17:48:21 +0200

    Add service with current user data

---
+++ src/srv/InstantJob/InstantJob.Api/Services/CurrentUserService.cs
@@ -0,0 +1,22 @@
+﻿using InstantJob.Core.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace InstantJob.Api.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor accessor)
+        {
+            if (int.TryParse(accessor.HttpContext?.User?.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value, out int id))
+            {
+                UserId = id;
+                Type = accessor.HttpContext?.User?.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
+            }
+        }
+
+        public int? UserId { get; }
+
+        public string Type { get; }
+    }
+}
---
+++ src/srv/InstantJob/InstantJob.Core/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace InstantJob.Core.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        public int? UserId { get; }
+
+        public string Type { get; }
+    }
+}
