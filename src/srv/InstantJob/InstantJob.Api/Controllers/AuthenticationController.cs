40286330b9f50bc1a9d3d245cb62c2a671e54e19
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-03 21:47:04 +0200

    Add authentication controller

---
+++ src/srv/InstantJob/InstantJob.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,69 @@
+﻿using InstantJob.Core.Common.Interfaces;
+using InstantJob.Core.Users.Dtos;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace InstantJob.Api.Controllers
+{
+    public class AuthenticationController : RoutedApiController
+    {
+        private readonly IUserManager userManager;
+
+        public AuthenticationController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task SignIn(UserAuthParams param)
+        {
+            var user = await userManager.ValidateCredentialsAsync(param);
+
+            if (user == null)
+            {
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return;
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, user.Type),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, new AuthenticationProperties { AllowRefresh = true});
+        }
+
+        [HttpPost]
+        public async Task SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        [HttpPost]
+        public async Task Register(UserRegisterParams param)
+        {
+            await userManager.CreateAsync(param);
+        }
+
+        [HttpPost]
+        public async Task ChangePassword(UserUpdatePasswordParams param)
+        {
+            await userManager.UpdatePasswordAsync(param);
+        }
+
+        [HttpPost]
+        public async Task UpdateInformation(UserUpdateInfoParams param)
+        {
+            await userManager.UpdateInformationAsync(param);
+        }
+    }
+}
