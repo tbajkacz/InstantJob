0f4bb931011bc2c77cfcad845e0c80a67c1dbbad
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-03 21:46:57 +0200

    Add controller base class

---
+++ src/srv/InstantJob/InstantJob.Api/Controllers/RoutedApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstantJob.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class RoutedApiController : ControllerBase
+    {
+    }
+}
