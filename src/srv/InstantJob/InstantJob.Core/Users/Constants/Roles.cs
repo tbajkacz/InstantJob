c12438fae8539d95016dcf2bb5a76490dc87c82a
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-06 21:39:39 +0200

    Add a static collection of roles

--- src/srv/InstantJob/InstantJob.Core/Users/Constants/Roles.cs
+++ src/srv/InstantJob/InstantJob.Core/Users/Constants/Roles.cs
@@ -1,9 +1,19 @@
-﻿namespace InstantJob.Core.Users.Constants
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace InstantJob.Core.Users.Constants
 {
     public class Roles
     {
         public const string Mandator = nameof(Mandator);
         public const string Mandatee = nameof(Mandatee);
         public const string Administrator = nameof(Administrator);
+
+        public static readonly IReadOnlyCollection<string> RolesCollection
+            = typeof(Roles).GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(f => f.IsLiteral && !f.IsInitOnly)
+                .Select(f => (string)f.GetRawConstantValue())
+                .ToList();
     }
 }
