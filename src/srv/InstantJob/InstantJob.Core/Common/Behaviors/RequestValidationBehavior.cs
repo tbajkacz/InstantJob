5cfbd1c68569247334517602dbf577e07d9ab19b
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-06 21:34:49 +0200

    Add RequestValidationBehavior for MediatR pipeline

---
+++ src/srv/InstantJob/InstantJob.Core/Common/Behaviors/RequestValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using InstantJob.Core.Common.Exceptions;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InstantJob.Core.Common.Behaviors
+{
+    public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var validationContext = new ValidationContext(request);
+
+            var validationFailures = _validators
+                .Select(v => v.Validate(validationContext))
+                .SelectMany(result => result.Errors.Where(f => f != null));
+
+            if (validationFailures.Count() != 0)
+            {
+                throw new ValidationFailedException(validationFailures);
+            }
+
+            return next();
+        }
+    }
+}
---
+++ src/srv/InstantJob/InstantJob.Core/Common/Exceptions/ValidationFailedException.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InstantJob.Core.Common.Exceptions
+{
+    public class ValidationFailedException : Exception
+    {
+        public IDictionary<string, string[]> Failures { get; }
+
+        public ValidationFailedException()
+            : base("Validation did not pass due to one or more failures")
+        {
+            Failures = new Dictionary<string, string[]>();
+        }
+
+        public ValidationFailedException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            foreach (var propertyName in failures.Select(f => f.PropertyName).Distinct())
+            {
+                Failures.Add(propertyName, failures.Where(x => x.PropertyName == propertyName)
+                                                   .Select(x => x.ErrorMessage)
+                                                   .ToArray());
+            }
+        }
+    }
+}
