5c1a616cfd644a3515f59989e3a8996a9ce6fae8
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-08 00:07:31 +0200

    Add user dto

---
+++ src/srv/InstantJob/InstantJob.Core/Users/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using InstantJob.Core.Common.Mappings;
+using InstantJob.Core.Users.Entities;
+
+namespace InstantJob.Core.Users.Dtos
+{
+    public class UserDto : IMapFrom<User>
+    {
+        public string Name { get; set; }
+               
+        public string Surname { get; set; }
+    }
+}
