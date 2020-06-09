1987943143a56a2cec684c27b7ee1195409fe689
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-09 23:54:42 +0200

    Remove BaseDomainEvent

--- src/srv/InstantJob/SharedKernel/Types/BaseDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SharedKernel.Types
-{
-    public abstract class BaseDomainEvent
-    {
-        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
-    }
-}
--- src/srv/InstantJob/SharedKernel/Types/BaseEntity.cs
+++ src/srv/InstantJob/SharedKernel/Types/BaseEntity.cs
@@ -5,7 +5,5 @@ namespace SharedKernel.Types
     public abstract class BaseEntity<TId>
     {
         public virtual TId Id { get; set; }
-
-        public virtual List<BaseDomainEvent> Events { get; set; } = new List<BaseDomainEvent>();
     }
 }
