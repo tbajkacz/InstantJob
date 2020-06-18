aa5c8ac569b402c0974b01edb0fb5d00e0e42a72
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-18 18:25:24 +0200

    Add job repository

---
+++ src/srv/InstantJob/InstantJob.Core/Common/Interfaces/IJobRepository.cs
@@ -0,0 +1,9 @@
+﻿using InstantJob.Core.Jobs.Entities;
+using System;
+
+namespace InstantJob.Core.Common.Interfaces
+{
+    public interface IJobRepository : IRepository<Job, Guid>
+    {
+    }
+}
---
+++ src/srv/InstantJob/InstantJob.Infrastructure/Data/NHibernateJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using InstantJob.Core.Common.Interfaces;
+using InstantJob.Core.Jobs.Entities;
+using NHibernate;
+using System;
+
+namespace InstantJob.Infrastructure.Data
+{
+    public class NHibernateJobRepository : NHibernateRepositoryBase<Job, Guid>, IJobRepository
+    {
+        public NHibernateJobRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
