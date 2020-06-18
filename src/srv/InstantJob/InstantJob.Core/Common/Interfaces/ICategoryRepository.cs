0ad24fc6db3ba6dfb5e6112e9712566344866b02
Tomasz Bajkacz <menidas98@gmail.com>
2020-06-18 18:25:38 +0200

    Add category repository

---
+++ src/srv/InstantJob/InstantJob.Core/Common/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using InstantJob.Core.Jobs.Entities;
+
+namespace InstantJob.Core.Common.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category, int>
+    {
+    }
+}
---
+++ src/srv/InstantJob/InstantJob.Infrastructure/Data/NHibernateCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using InstantJob.Core.Common.Interfaces;
+using InstantJob.Core.Jobs.Entities;
+using NHibernate;
+
+namespace InstantJob.Infrastructure.Data
+{
+    public class NHibernateCategoryRepository : NHibernateRepositoryBase<Category, int>, ICategoryRepository
+    {
+        public NHibernateCategoryRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
