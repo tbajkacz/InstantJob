4372547814f93bcfe136d8268ca6101e8c668abf
Tomasz Bajkacz <menidas98@gmail.com>
2020-07-16 16:40:07 +0200

    Change id values to start from 1

--- src/srv/InstantJob/InstantJob.Domain/Jobs/Constants/Difficulty.cs
+++ src/srv/InstantJob/InstantJob.Domain/Jobs/Constants/Difficulty.cs
@@ -1,13 +1,12 @@
 ﻿using InstantJob.Domain.Common;
-using System.Linq;
 
 namespace InstantJob.Domain.Jobs.Constants
 {
     public class Difficulty : Enumeration
     {
-        public static readonly Difficulty Beginner = new Difficulty(0, "Beginner");
-        public static readonly Difficulty Intermediate = new Difficulty(1, "Intermediate");
-        public static readonly Difficulty Expert = new Difficulty(2, "Expert");
+        public static readonly Difficulty Beginner = new Difficulty(1, "Beginner");
+        public static readonly Difficulty Intermediate = new Difficulty(2, "Intermediate");
+        public static readonly Difficulty Expert = new Difficulty(3, "Expert");
 
         protected Difficulty(int id, string name)
             : base(id, name)
