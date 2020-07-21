1a7a459de542d88fc92ec984195d37cd20911984
Tomasz Bajkacz <menidas98@gmail.com>
2020-07-21 13:25:01 +0200

    Add UnitOfWorkCommitBehavior

---
+++ src/srv/InstantJob/BuildingBlocks/InstantJob.BuildingBlocks.Application/MediatR/UnitOfWorkCommitBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using InstantJob.BuildingBlocks.Application.Interfaces;
+using MediatR;
+
+namespace InstantJob.BuildingBlocks.Application.MediatR
+{
+    public class UnitOfWorkCommitBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TRequest>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IUnitOfWork uow;
+
+        public UnitOfWorkCommitBehavior(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public async Task<TRequest> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TRequest> next)
+        {
+            if (!uow.Active)
+            {
+                throw new Exception("Unit of work connection is already closed");
+            }
+
+            var nextRequest = await next();
+
+            await uow.CommitAsync();
+
+            return nextRequest;
+        }
+    }
+}
