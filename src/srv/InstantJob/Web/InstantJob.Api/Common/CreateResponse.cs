db49c093104cae6c1ccd2da7d4315f3ffd1187b4
Tomasz Bajkacz <menidas98@gmail.com>
2020-07-25 21:36:15 +0200

    Add CreateResponse

---
+++ src/srv/InstantJob/Web/InstantJob.Api/Common/CreateResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace InstantJob.Web.Api.Common
+{
+    public class CreateResponse<T>
+    {
+        public T Id { get; }
+
+        public CreateResponse(T id)
+        {
+            Id = id;
+        }
+    }
+}
