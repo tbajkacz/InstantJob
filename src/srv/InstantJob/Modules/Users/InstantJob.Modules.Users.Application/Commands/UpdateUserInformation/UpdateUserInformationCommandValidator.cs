862b7da4b4794f76c1a113ebd611b7ce690a2e77
Tomasz Bajkacz <menidas98@gmail.com>
2020-07-25 22:01:58 +0200

    Add validator for UpdateUserInformationCommand

---
+++ src/srv/InstantJob/Modules/Users/InstantJob.Modules.Users.Application/Commands/UpdateUserInformation/UpdateUserInformationCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace InstantJob.Modules.Users.Application.Commands.UpdateUserInformation
+{
+    public class UpdateUserInformationCommandValidator : BaseUserValidator<UpdateUserInformationCommand>
+    {
+        public UpdateUserInformationCommandValidator()
+        {
+            RuleForName(x => x.Name);
+            RuleForSurname(x => x.Surname);
+        }
+    }
+}
