e40a716b671f6d646b8f91dee95c9d6de1aa7c5e
Tomasz Bajkacz <menidas98@gmail.com>
2020-08-20 12:38:52 +0200

    Add role property to user dto

--- src/srv/InstantJob/Modules/Users/InstantJob.Modules.Users.Application/Users/Queries/GetUserById/UserDetailsDto.cs
+++ src/srv/InstantJob/Modules/Users/InstantJob.Modules.Users.Application/Users/Queries/GetUserById/UserDetailsDto.cs
@@ -1,18 +1,21 @@
 ﻿using InstantJob.BuildingBlocks.Application.Automapper;
+using InstantJob.BuildingBlocks.Domain;
 using InstantJob.Modules.Users.Domain.Users;
 
 namespace InstantJob.Modules.Users.Application.Users.Queries.GetUserById
 {
     public class UserDetailsDto : IMapFrom<User>
     {
-        public string Name { get; protected set; }
+        public string Name { get; set; }
 
-        public string Surname { get; protected set; }
+        public string Surname { get; set; }
 
-        public int? Age { get; protected set; }
+        public int? Age { get; set; }
 
-        public string Email { get; protected set; }
+        public string Email { get; set; }
 
-        public string Picture { get; protected set; }
+        public string Picture { get; set; }
+
+        public Role Role { get; set; }
     }
 }
