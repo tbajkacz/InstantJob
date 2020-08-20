12c2d28742266207ac6eb9f4ad3fa45c1bb3507d
Tomasz Bajkacz <menidas98@gmail.com>
2020-08-20 12:35:43 +0200

    Move user endpoints to UsersController

--- src/srv/InstantJob/Web/InstantJob.Api/Controllers/AuthenticationController.cs
+++ src/srv/InstantJob/Web/InstantJob.Api/Controllers/AuthenticationController.cs
@@ -64,27 +64,5 @@ namespace InstantJob.Web.Api.Controllers
         {
             return await mediator.Send(new GetUserDetailsQuery());
         }
-
-        /// <summary>
-        /// Changes current users password
-        /// </summary>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        [HttpPatch("user/password")]
-        public async Task ChangePassword(ChangeUserPasswordCommand command)
-        {
-            await mediator.Send(command);
-        }
-
-        /// <summary>
-        /// Updates current users information
-        /// </summary>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        [HttpPatch("user/info")]
-        public async Task UpdateInformation(UpdateUserInformationCommand command)
-        {
-            await mediator.Send(command);
-        }
     }
 }
---
+++ src/srv/InstantJob/Web/InstantJob.Api/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using InstantJob.Modules.Users.Application.Users.Commands.ChangeUserPassword;
+using InstantJob.Modules.Users.Application.Users.Commands.UpdateUserInformation;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace InstantJob.Web.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Changes current users password
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPatch("user/password")]
+        public async Task ChangePassword(ChangeUserPasswordCommand command)
+        {
+            await mediator.Send(command);
+        }
+
+        /// <summary>
+        /// Updates current users information
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPatch("user/info")]
+        public async Task UpdateInformation(UpdateUserInformationCommand command)
+        {
+            await mediator.Send(command);
+        }
+    }
+}
